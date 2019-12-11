fc4a67f2f9cf19b2644af37ef083648f0e97b14d
Ben Baumer <ben.baumer@gmail.com>
2019-12-11 15:15:16 -0500

    fixed .onAttach()

--- R/attach.R
+++ R/attach.R
@@ -2,28 +2,25 @@
 # Is this basically what this function is supposed to look like?
 # Groups will be assigned later, so these are blank for now.
 
-#' @export
-.onAttach <- function(libname, pkgname){
+.onAttach <- function(libname, pkgname) {
   
-  fertile_group <- c()
+  fertile_group <- c("bbaumer")
     
   control_group <- c()
     
   non_participants <- c() 
   
   username <- Sys.getenv("LOGNAME")
-  
-  if (username %in% fertile_group){
+
+  if (username %in% fertile_group) {
     
-    if ("fertile" %in% rownames(installed.packages()) == FALSE) {
-      suppressMessages(install_github("baumer-lab/fertile"))
+    if (!"fertile" %in% rownames(utils::installed.packages())) {
+      suppressMessages(
+        remotes::install_github("baumer-lab/fertile", dependencies = TRUE)
+      )
     }
-    
     suppressMessages(library(fertile))
-    help(package = "fertile")
   }
-  
-  
 }
   
   
