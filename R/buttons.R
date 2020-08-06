07c918ee778131c27950ee99991be417ff52be76
Ben Baumer <ben.baumer@gmail.com>
2020-08-06 10:55:02 -0400

    added buttons

---
+++ R/buttons.R
@@ -0,0 +1,24 @@
+#' Standard Buttons
+#' @export
+#' @param x text of the standard
+#' @param ... arguments passed to \code{\link[bsplus]{bs_button}}
+#' @examples 
+#' standard_button("aesthetics")
+#' standard_button("markdown")
+
+standard_button <- function(x, ...) {
+  type <- dplyr::case_when(
+    tolower(x) %in% c("aesthetics", "context") ~ "primary",
+    tolower(x) %in% c("wrangling", "reshape", "iteration", "function") ~ "success",
+    tolower(x) %in% c("query") ~ "info",
+    tolower(x) %in% c("spatial", "leaflet") ~ "warning",
+    tolower(x) %in% c("markdown", "github") ~ "danger",
+    TRUE ~ "default"
+  ) 
+  bsplus::bs_button(
+    toupper(x), 
+    button_type = type, 
+    button_size = "small",
+    ...
+  )
+}
