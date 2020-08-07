fa6792f44bb18c1df1f4d0f0cdb90a2b81f5e3d7
Ben Baumer <ben.baumer@gmail.com>
2020-08-07 09:57:47 -0400

    added relational standard

--- R/buttons.R
+++ R/buttons.R
@@ -9,7 +9,7 @@
 standard_button <- function(x, ...) {
   type <- dplyr::case_when(
     tolower(x) %in% c("aesthetics", "context") ~ "primary",
-    tolower(x) %in% c("wrangling", "reshape", "iteration", "function") ~ "success",
+    tolower(x) %in% c("wrangling", "relational", "reshape", "iteration", "function") ~ "success",
     tolower(x) %in% c("query") ~ "info",
     tolower(x) %in% c("spatial", "leaflet") ~ "warning",
     tolower(x) %in% c("markdown", "github") ~ "danger",
