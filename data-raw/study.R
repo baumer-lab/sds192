b529e6b159a24541cc31fa3f7afc3ef99f56a5c1
beanumber <ben.baumer@gmail.com>
2020-09-14 10:17:27 -0400

    updated study participants

--- data-raw/study.R
+++ data-raw/study.R
@@ -6,7 +6,28 @@ students <- readr::read_csv("/tmp/SDS 192 Fall 2020 Study Consent (Responses) - 
   rename(login = 5) %>%
   filter(do_you_consent_to_participate_in_this_study == "Yes") %>%
   mutate(
-    user = str_trim(login),
+    user = str_remove(login, "^\\[1\\]"),
+    user = str_trim(user),
     user = str_remove_all(user, '\\"'),
-    user = str_remove(user, "^\\[1\\] ")
+    hash = map_chr(user, digest::digest, algo = "md5"),
+    seed = runif(n = nrow(.)),
   )
+
+participants <- students %>%
+  filter(user > "") %>%
+  filter(user != "+") %>%
+  filter(!grepl("I never", user)) %>%
+  arrange(desc(seed)) %>%
+  mutate(control = rep(c(TRUE, FALSE), length.out = nrow(.))) %>%
+  select(control, hash)
+
+# paste into .attach.R
+participants %>%
+  filter(control) %>%
+  pull(hash)
+
+participants %>%
+  filter(!control) %>%
+  pull(hash)
+
+# readr::write_csv(students, "/tmp/students.csv")
