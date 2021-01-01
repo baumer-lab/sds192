8d2165a0ca537a91763b6edd653fd58018e852bf
beanumber <ben.baumer@gmail.com>
2021-01-01 11:59:40 -0500

    added dbConnect function; close #5

---
+++ R/scidb.R
@@ -0,0 +1,32 @@
+#' Connections to scidb
+#' @description Connect to the \code{scidb} server at Smith College.
+#' @param dbname the name of the database to which you want to connect
+#' @param ... arguments passed to \code{\link[RMySQL]{dbConnect}}
+#' @details This is a public, read-only account. Any abuse will be considered a 
+#' hostile act. 
+#' @export
+#' @return A \code{\link[RMySQL]{MySQLConnection-class}} object
+#' 
+#' @seealso \code{\link[RMySQL]{MySQLConnection-class}}
+#' @examples 
+#' db_air <- dbConnect_scidb("airlines")
+#' if (require(RMySQL)) {
+#'   summary(db_air)
+#'   dbListTables(db_air)
+#'   if (require(dplyr)) {
+#'     tbl(db_air, "airports")
+#'   }
+#'   dbDisconnect(db)
+#' }
+
+dbConnect_scidb <- function(dbname, ...) {
+  RMySQL::dbConnect(
+    RMySQL::MySQL(), 
+    dbname = dbname, 
+    host = "scidb.smith.edu", 
+    user = "sds192", 
+    password = "DSismfc@S", 
+    ... = ...
+  )
+}
+
--- tests/testthat/test-attach.R
+++ tests/testthat/test-attach.R
@@ -3,3 +3,13 @@ test_that("experiment works", {
   expect_false(sds192:::is_experimental("ambertin"))
   expect_false(sds192:::is_experimental("blahblahblah"))
 })
+
+
+test_that("scidb works", {
+  db <- dbConnect_scidb("airlines")
+  expect_s4_class(db, "MySQLConnection")
+  x <- suppressWarnings(
+    dplyr::tbl(db, "airports")
+  )
+  expect_s3_class(x, c("tbl_MySQLConnection", "tbl_dbi", "tbl_sql", "tbl"))
+})
