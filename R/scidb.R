8bdcc9ae8e080d1853f0d7c7ea613838f992237f
beanumber <ben.baumer@gmail.com>
2021-01-06 16:22:20 -0500

    fixed R CMD check notes

--- R/scidb.R
+++ R/scidb.R
@@ -1,7 +1,7 @@
 #' Connections to scidb
 #' @description Connect to the \code{scidb} server at Smith College.
 #' @param dbname the name of the database to which you want to connect
-#' @param ... arguments passed to \code{\link[RMySQL]{dbConnect}}
+#' @param ... arguments passed to \code{\link[DBI]{dbConnect}}
 #' @details This is a public, read-only account. Any abuse will be considered a 
 #' hostile act. 
 #' @export
@@ -16,11 +16,11 @@
 #'   if (require(dplyr)) {
 #'     tbl(db_air, "airports")
 #'   }
-#'   dbDisconnect(db)
+#'   dbDisconnect(db_air)
 #' }
 
 dbConnect_scidb <- function(dbname, ...) {
-  RMySQL::dbConnect(
+  DBI::dbConnect(
     RMySQL::MySQL(), 
     dbname = dbname, 
     host = "scidb.smith.edu", 
